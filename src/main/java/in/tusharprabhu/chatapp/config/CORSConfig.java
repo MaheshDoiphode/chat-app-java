8282224b1a353a46219b7b0caddc0977003decd4
Mahesh <maheshdoiphode0007@gmail.com>
2024-06-11 12:00:50 +0530

    initial commit

---
+++ src/main/java/in/tusharprabhu/chatapp/config/CORSConfig.java
@@ -0,0 +1,17 @@
+package in.tusharprabhu.chatapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CORSConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
+    }
+}
